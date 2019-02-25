f0b28a6b4cb689ea0165b3b39136fda3f7f4538a
Lian Li <lianl@rvigroup.com>
2019-02-25 17:38:32 -0500

    customer list

---
+++ WebContent/WEB-INF/view/list-customers.jsp
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>List Customers</title>
+</head>
+<body>
+
+List Customers -comming soon....
+
+</body>
+</html>
---
+++ src/com/springdemo/dao/CustomerDAOImpl.java
@@ -0,0 +1,40 @@
+package com.springdemo.dao;
+
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.springdemo.entity.Customer;
+
+@Repository
+public class CustomerDAOImpl implements CustomerDAO {
+
+	//need to inject the session factory
+	@Autowired
+	public SessionFactory sessionFactory;
+	
+	
+	@Override
+	@Transactional
+	public List<Customer> getCustomers() {
+		
+		// get the current hibernate session
+		Session currentSession = sessionFactory.getCurrentSession();
+		
+		// create a query
+		Query<Customer> theQuery = currentSession.createQuery("form Customer", Customer.class);
+		
+		//execute query and get result list
+		List<Customer> customers = theQuery.getResultList();
+		
+		//return results
+		return customers;
+	}
+
+}
